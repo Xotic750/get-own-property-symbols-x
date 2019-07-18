6bc25a08e22e7b9a7e03de76ea3ebee259d31ec3
Graham Fairweather <xotic750@gmail.com>
2019-07-18 20:56:23 +0200

    :bookmark: v2.0.7
    Auto build

--- dist/get-own-property-symbols-x.js
+++ dist/get-own-property-symbols-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-18T00:04:01.805Z",
+  "date": "2019-07-18T18:56:17.843Z",
   "describe": "",
   "description": "Creates an array of all symbol properties found directly upon a given object.",
   "file": "get-own-property-symbols-x.js",
   "hash": "7c50820ac43a2748b350",
   "license": "MIT",
-  "version": "2.0.6"
+  "version": "2.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
